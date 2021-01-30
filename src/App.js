3e83c8c207eea5893c1174f24317c975f88fc962
Marvin T <contact@marvinkturner.com>
2021-01-29 23:58:56 -0500

    feat: pass data from source to componentt

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div className='container'>
+      <h3>Questions and answers about login</h3>
+      <section className='info'>
+        {questions.map((q) => {
+          return <SingleQuestion key={q.id} {...q} />;
+        })}
+      </section>
+    </div>
+  );
+}
+
+export default App;
